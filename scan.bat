fb5a858dc02f226e107bbe867a26fd81adbee632
Nvtai <buiblue123@gmail.com>
2025-02-11 15:23:40 +0700

    add .gitignore

--- scan.bat
+++
@@ -1,8 +0,0 @@
-sonar-scanner ^
-  -Dsonar.projectKey=main ^
-  -Dsonar.sources=src ^
-  -Dsonar.host.url=http://localhost:9000 ^
-  -Dsonar.token=sqp_581e540af1fd9d91abbf2e13fa194347ae58f322 ^
- -Dsonar.java.binaries=build/web/WEB-INF/classes ^
-  -Dsonar.login=admin ^
-  -Dsonar.password=Hoang11221212345@
